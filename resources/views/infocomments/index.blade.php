ae7a25a938ae6bbc9837f2558f6357a130715355
Francesco Scarnera <francesco.scarnera3@gmail.com>
2020-06-23 18:39:08 +0200

    minority bug

--- resources/views/infocomments/index.blade.php
+++ resources/views/infocomments/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     @foreach ( $comment->InfoComments as $InfoComment )
-    <h3>{{ $InfoComment->phone }}</h3>
-    <h3>{{ $InfoComment->address }}</h3>
-    <img src="{{ $InfoComment->avatar }}" alt="">
+        <h3>{{ $InfoComment->phone }}</h3>
+        <h3>{{ $InfoComment->address }}</h3>
+        <img src="{{ $InfoComment->avatar }}" alt="">
     @endforeach 
 @endsection

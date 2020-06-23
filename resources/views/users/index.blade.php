81e1f0e4ec459d5901742e69d2cef46d1c5844ad
Francesco Scarnera <francesco.scarnera3@gmail.com>
2020-06-23 17:05:33 +0200

    fix

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -15,10 +15,16 @@
                     <li>
                         <h4>{{ $post->title }}</h4>
                         <p>{{ $post->body }}</p>
+                        <h3>Comments:</h3>
+                        @foreach( $post->comments as $comment )
+                            <h4>{{ $comment->title }}</h4>
+                            <p>{{ $comment->body }}</p>
+                            <h5>{{ $comment->name }}</h5>
+                        @endforeach
                     </li>
                 @endforeach
             </ul>
-            <h3>Comments</h3>
+            {{-- <h3>Comments</h3>
             <ul>
                 @foreach( $post->comments as $comment )
                 <li>
@@ -27,7 +33,7 @@
                     <h5>{{ $comment->name }}</h5>
                 </li>
                 @endforeach
-            </ul>
+            </ul> --}}
         </div>
         @if ( ! $loop->last )
             <hr>
